116bcec04f608120b1cd22c45b509dc78d2c90ce
SUSHANT PATHARE <sushantsp14@gmail.com>
2025-02-08 22:54:23 +0530

    HealthCheck Route Added

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,10 @@ app.get('/', (req, res) => {
     res.send('Api Working');
 })
 
+//Router imports
+import healthCheckRoute from './routes/healthCheck.route.js'
+app.use('/api/v1/healthCheck', healthCheckRoute)
+
 app.listen(PORT, () => {
     console.log(`Server started at ${PORT}`);
 })
